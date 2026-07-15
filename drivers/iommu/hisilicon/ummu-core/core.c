OERV-RVCI/rvck-olk

commit 9db5eb9c30ff4e72435351758c9a6a3b11af894a

iommu/ummu-core: core interfaces for ummu drivers
ubmmu inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID3QMR

----------------------------------------------

This patch provides register APIs to the ummu drivers and public APIs to
ub drivers.

The ummu device registration split into two steps, init and register.
ummu device must implement ummu_core_device and init it by
`ummu_core_device_init`. It initializes the core device and allocates a
tid manager to handle the tid.

Multiple ummu devices can register to ummu core. but only one device can
take the charge of ubus and ub device request.

              Device
                ^
                |
               Ubus
                ^
----------------|-------------------
|               |                  |
|         global core device       |
|         ·                        |
|         ·                        |
|  core_dev1 ---> ... -> core_dev2 |
|    |                        |    |
|    - ---<------------<------|    |
|                                  |
------------------------------------

UMMU-CORE also provide APIs to Users
1. SVA API
2. tid related query API.

Signed-off-by: Yuhao Zhang <yuhao.zhang@huawei.com>
Signed-off-by: Yanlong Zhu <zhuyanlong3@huawei.com>
Signed-off-by: Xiaowei Jiang <jiangxiaowei666@huawei.com>
Signed-off-by: Lei Gong <gonglei25@huawei.com>

drivers/iommu/hisilicon/ummu-core/core.c
@@ -8,52 +8,290 @@
 #include <linux/module.h>
 #include <ub/ubus/ubus.h>
 
+#include "../../iommu-priv.h"
 #include "ummu_core_priv.h"
+
+static struct ummu_tid_manager *
+ummu_core_alloc_tid_manager(struct ummu_core_device *core_device,
+			    const struct tid_ops *ops, u32 max,
+			    u32 min)
+{
+	struct ummu_tid_manager *manager;
+
+	if (!ops || !ops->alloc_tid_manager || !ops->free_tid_manager) {
+		dev_err(core_device->iommu.dev, "invalid ops.\n");
+		return NULL;
+	}
+	manager = ops->alloc_tid_manager(core_device, max, min);
+	if (!manager) {
+		dev_err(core_device->iommu.dev, "failed to alloc tid manager.\n");
+		return NULL;
+	}
+	manager->ops = ops;
+	manager->max_tid = max;
+	manager->min_tid = min;
+	return manager;
+}
+
+static void ummu_core_free_tid_manager(struct ummu_tid_manager *manager)
+{
+	manager->ops->free_tid_manager(manager);
+}
+
+static int set_global_device(struct ummu_core_device *core_device)
+{
+	if (!core_device->ops || !core_device->ops->add_eid ||
+		!core_device->ops->del_eid || !core_device->iommu.ops) {
+		pr_err("invalid ops.\n");
+		return -EOPNOTSUPP;
+	}
+	if (global_core_device)
+		return -EEXIST;
+
+	ub_bus_type_iommu_ops_set(core_device->iommu.ops);
+	ummu_flush_cached_eid(core_device);
+	global_core_device = core_device;
+
+	return 0;
+}
+
+static void reset_global_device(void)
+{
+	ub_bus_type_iommu_ops_set(NULL);
+	global_core_device = NULL;
+}
+
+static void ummu_core_root_release(struct device *dev)
+{
+	pr_debug("%s root release\n", dev_name(dev));
+	kfree(dev);
+}
+
+static int ummu_core_device_add_sysfs(struct ummu_core_device *ummu_core)
+{
+	struct device *root;
+	int ret;
+
+	root = kzalloc(sizeof(*root), GFP_KERNEL);
+	if (!root)
+		return -ENOMEM;
+
+	root->init_name = dev_name(ummu_core->iommu.dev);
+	root->parent = &tid_bus;
+	root->release = ummu_core_root_release;
+
+	ret = device_register(root);
+	if (ret) {
+		pr_err("ummu_core_device root register err!\n");
+		put_device(root);
+		return ret;
+	}
+
+	ummu_core->ummu_core_root = root;
+	return 0;
+}
+
+static void ummu_core_device_remove_sysfs(struct ummu_core_device *ummu_core)
+{
+	struct device *root;
+
+	root = ummu_core->ummu_core_root;
+	ummu_core->ummu_core_root = NULL;
+	device_unregister(root);
+}
+
+int ummu_core_device_init(struct ummu_core_device *ummu_core,
+			  struct ummu_core_init_args *args)
+{
+	struct ummu_tid_manager *tid_manager;
+
+	if (!args->iommu_ops)
+		return -EINVAL;
+
+	if (args->tid_args.tid_ops) {
+		tid_manager = ummu_core_alloc_tid_manager(ummu_core,
+						args->tid_args.tid_ops,
+						args->tid_args.max_tid,
+						args->tid_args.min_tid);
+		if (!tid_manager)
+			return -ENOMEM;
+
+		ummu_core->tid_manager = tid_manager;
+	}
+	ummu_core->iommu.ops = args->iommu_ops;
+	ummu_core->ops = args->core_ops;
+	if (args->hwdev)
+		ummu_core->iommu.fwnode = dev_fwnode(args->hwdev);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(ummu_core_device_init);
+
+void ummu_core_device_deinit(struct ummu_core_device *ummu_core)
+{
+	if (ummu_core->tid_manager)
+		ummu_core_free_tid_manager(ummu_core->tid_manager);
+
+	ummu_core->tid_manager = NULL;
+	ummu_core->ops = NULL;
+	ummu_core->iommu.ops = NULL;
+	ummu_core->iommu.fwnode = NULL;
+}
+EXPORT_SYMBOL_GPL(ummu_core_device_deinit);
+
+int ummu_core_device_register(struct ummu_core_device *ummu_core,
+			      enum ummu_register_type type)
+{
+	int ret;
+
+	if (type >= REGISTER_TYPE_MAX)
+		return -EINVAL;
+
+	mutex_lock(&global_device_lock);
+	switch (type) {
+	case REGISTER_TYPE_GLOBAL: /* register as the global iommu device */
+		ret = set_global_device(ummu_core);
+		if (ret)
+			goto out_unlock;
+		fallthrough;
+	case REGISTER_TYPE_NORMAL: /* register to the iommu framework */
+		ret = iommu_device_register(&ummu_core->iommu,
+						ummu_core->iommu.ops,
+						NULL);
+		if (ret)
+			goto out_reset_device;
+
+		ret = ummu_core_device_add_sysfs(ummu_core);
+		if (ret) {
+			dev_err(ummu_core->iommu.dev, "ummu core device add sysfs failed.\n");
+			iommu_device_unregister(&ummu_core->iommu);
+			goto out_reset_device;
+		}
+
+		list_add_tail(&ummu_core->list, &core_device_list);
+		break;
+	default:
+		ret = -EOPNOTSUPP;
+		break;
+	}
+	mutex_unlock(&global_device_lock);
+	return ret;
+
+out_reset_device:
+	if (type == REGISTER_TYPE_GLOBAL)
+		reset_global_device();
+out_unlock:
+	mutex_unlock(&global_device_lock);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(ummu_core_device_register);
+
+void ummu_core_device_unregister(struct ummu_core_device *ummu_core)
+{
+	struct ummu_core_device *entry, *next;
+
+	mutex_lock(&global_device_lock);
+	if (global_core_device == ummu_core)
+		reset_global_device();
+
+	list_for_each_entry_safe(entry, next, &core_device_list, list)
+		if (entry == ummu_core) {
+			iommu_device_unregister(&entry->iommu);
+			list_del(&entry->list);
+			break;
+		}
+
+	ummu_core_device_remove_sysfs(ummu_core);
+	mutex_unlock(&global_device_lock);
+}
+EXPORT_SYMBOL_GPL(ummu_core_device_unregister);
 
 int ummu_core_get_resource(struct iommu_sva *sva, struct resource_args *args)
 {
 	struct ummu_core_device *core_device;
-	const struct ummu_core_ops *ops;
 
-	if (!sva || !sva->dev) {
-		pr_err("invalid params.\n");
-		return -EINVAL;
-	}
 	core_device = iommu_get_iommu_dev(sva->dev, struct ummu_core_device, iommu);
-	ops = core_device->ops;
-	if (!ops || !ops->get_resource) {
-		pr_err("invalid ops.\n");
+	if (!core_device->ops || !core_device->ops->get_resource)
 		return -ENODEV;
-	}
 
-	return ops->get_resource(to_ummu_base_domain(sva->handle.domain), args);
+	return core_device->ops->get_resource(to_ummu_base_domain(sva->handle.domain), args);
 }
 
 void ummu_core_put_resource(struct iommu_sva *sva, struct resource_args *args)
 {
 	struct ummu_core_device *core_device;
-	const struct ummu_core_ops *ops;
 
-	if (!sva || !sva->dev) {
-		pr_err("invalid params.\n");
-		return;
-	}
 	core_device = iommu_get_iommu_dev(sva->dev, struct ummu_core_device, iommu);
-	ops = core_device->ops;
-	if (!ops || !ops->put_resource) {
-		pr_err("invalid ops.\n");
+	if (!core_device->ops || !core_device->ops->put_resource)
 		return;
-	}
 
-	ops->put_resource(to_ummu_base_domain(sva->handle.domain), args);
+	core_device->ops->put_resource(to_ummu_base_domain(sva->handle.domain), args);
 }
 
-int ummu_get_tid(struct device *dev, struct iommu_sva *sva, uint32_t *tidp)
+struct iommu_sva *ummu_sva_bind_device(struct device *dev, struct mm_struct *mm,
+				       struct ummu_param *drvdata)
+{
+	struct iommu_sva *sva =
+		iommu_sva_bind_device_isolated(dev, mm, drvdata);
+
+	if (IS_ERR(sva))
+		return NULL;
+	return sva;
+}
+EXPORT_SYMBOL_GPL(ummu_sva_bind_device);
+
+bool ummu_is_ksva(struct iommu_domain *domain)
+{
+	return iommu_is_ksva_domain(domain);
+}
+EXPORT_SYMBOL_GPL(ummu_is_ksva);
+
+bool ummu_is_sva(struct iommu_domain *domain)
+{
+	return domain->mm && !iommu_is_ksva_domain(domain);
+}
+EXPORT_SYMBOL_GPL(ummu_is_sva);
+
+struct iommu_sva *ummu_ksva_bind_device(struct device *dev,
+					struct ummu_param *drvdata)
+{
+	struct iommu_sva *sva = iommu_ksva_bind_device(dev, (void *)drvdata);
+
+	if (IS_ERR(sva))
+		return NULL;
+	return sva;
+}
+EXPORT_SYMBOL_GPL(ummu_ksva_bind_device);
+
+void ummu_sva_unbind_device(struct iommu_sva *sva)
+{
+	iommu_sva_unbind_device_isolated(sva);
+}
+EXPORT_SYMBOL_GPL(ummu_sva_unbind_device);
+
+void ummu_ksva_unbind_device(struct iommu_sva *sva)
+{
+	iommu_ksva_unbind_device(sva);
+}
+EXPORT_SYMBOL_GPL(ummu_ksva_unbind_device);
+
+int ummu_sva_grant_range(struct iommu_sva *sva, void *va, size_t size, int perm,
+			 void *cookie)
+{
+	return iommu_sva_grant(sva, va, size, perm, cookie);
+}
+EXPORT_SYMBOL_GPL(ummu_sva_grant_range);
+
+int ummu_sva_ungrant_range(struct iommu_sva *sva, void *va, size_t size,
+			   void *cookie)
+{
+	return iommu_sva_ungrant(sva, va, size, cookie);
+}
+EXPORT_SYMBOL_GPL(ummu_sva_ungrant_range);
+
+int ummu_get_tid(struct device *dev, struct iommu_sva *sva, u32 *tidp)
 {
 	struct iommu_domain *domain;
-
-	if (!dev || !tidp)
-		return -EINVAL;
 
 	if (sva)
 		domain = sva->handle.domain;
@@ -66,8 +304,94 @@ int ummu_get_tid(struct device *dev, struct iommu_sva *sva, uint32_t *tidp)
 	*tidp = to_ummu_base_domain(domain)->tid;
 	return 0;
 }
+EXPORT_SYMBOL_GPL(ummu_get_tid);
+
+struct iommu_domain *ummu_core_get_domain_by_tid(struct device *dev,
+						 u32 tid)
+{
+	struct iommu_attach_handle *attach_handle;
+	struct iommu_domain *domain;
+
+	attach_handle = iommu_attach_handle_get(dev->iommu_group, tid,
+						IOMMU_DOMAIN_SVA);
+	if (IS_ERR(attach_handle))
+		domain = iommu_get_domain_for_dev(dev);
+	else
+		domain = attach_handle->domain;
+
+	if (!domain)
+		return NULL;
+
+	if (to_ummu_base_domain(domain)->tid != tid)
+		return NULL;
+
+	return domain;
+}
+EXPORT_SYMBOL_GPL(ummu_core_get_domain_by_tid);
+
+int ummu_core_invalidate_cfg_table(u32 tid)
+{
+	struct iommu_domain *domain;
+	struct device *dev;
+	int ret;
+
+	mutex_lock(&global_device_lock);
+	if (!global_core_device) {
+		ret = -ENODEV;
+		goto out_unlock;
+	}
+	dev = ummu_core_get_device(global_core_device, tid);
+	if (!dev) {
+		ret = -ENODEV;
+		goto out_unlock;
+	}
+	domain = ummu_core_get_domain_by_tid(dev, tid);
+	if (!domain) {
+		ret = -EINVAL;
+		goto out_put_device;
+	}
+	if (!global_core_device->ops || !global_core_device->ops->invalidate_cfg) {
+		ret = -ENODEV;
+		goto out_put_device;
+	}
+	ret = global_core_device->ops->invalidate_cfg(to_ummu_base_domain(domain));
+
+out_put_device:
+	ummu_core_put_device(dev);
+out_unlock:
+	mutex_unlock(&global_device_lock);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(ummu_core_invalidate_cfg_table);
+
+static int __init ummu_core_init(void)
+{
+	int ret;
+
+	ret = tid_misc_init();
+	if (ret)
+		return ret;
+
+	ret = tdev_init();
+	if (ret)
+		tid_misc_exit();
+
+	return ret;
+}
+
+static void __exit ummu_core_exit(void)
+{
+	tdev_exit();
+	tid_misc_exit();
+}
+
+module_init(ummu_core_init);
+module_exit(ummu_core_exit);
 
 MODULE_IMPORT_NS(UMMU_CORE_INTERNAL);
+#if IS_MODULE(CONFIG_UB_UMMU_CORE_DRIVER)
+MODULE_IMPORT_NS(IOMMUFD_INTERNAL); /* get iommu_attach_handle_get */
+#endif
 MODULE_DESCRIPTION("UMMU Framework");
 MODULE_AUTHOR("HUAWEI TECHNOLOGIES CO., LTD.");
 MODULE_LICENSE("GPL");

drivers/iommu/hisilicon/ummu-core/ummu_core_priv.h
@@ -23,6 +23,7 @@ struct tid_dev {
 };
 
 /* private definition */
+#define UMMU_CORE_VER_NO "1.0.0"
 #define to_tid_dev(n) container_of(to_platform_device(n), struct tid_dev, pdev)
 
 /* private variable */
@@ -36,7 +37,6 @@ void ummu_flush_cached_eid(struct ummu_core_device *core_device);
 struct device *ummu_alloc_tdev(struct tdev_attr *attr, u32 *ptid);
 int ummu_core_get_resource(struct iommu_sva *sva, struct resource_args *args);
 void ummu_core_put_resource(struct iommu_sva *sva, struct resource_args *args);
-int ummu_get_tid(struct device *dev, struct iommu_sva *sva, uint32_t *tidp);
 void setup_tdev_dma_ops(struct device *dev, bool coherent);
 
 #endif /* __UMMU_CORE_PRIV_H__ */

include/linux/ummu_core.h
@@ -21,6 +21,10 @@
 #define UMMU_NO_TID 0U
 #define UMMU_INVALID_TID UB_MAX_TID
 
+#define UMMU_DEV_WRITE 1
+#define UMMU_DEV_READ 2
+#define UMMU_DEV_ATOMIC 4
+
 enum eid_type {
 	EID_NONE = 0,
 	EID_BYPASS,
@@ -45,6 +49,12 @@ enum default_tid_ops_types {
 	PASID_OPS,
 	DEFAULT_OPS,
 	TID_OPS_MAX,
+};
+
+enum ummu_register_type {
+	REGISTER_TYPE_GLOBAL,
+	REGISTER_TYPE_NORMAL,
+	REGISTER_TYPE_MAX,
 };
 
 struct iova_slot;
@@ -150,6 +160,9 @@ struct tdev_attr {
  * @put_resource: Put resource for SVA.
  * @add_eid: Add EID to the UMMU device.
  * @del_eid: Add EID to the UMMU device.
+ * @invalidate_cfg: invalid configuration table by tid.
+ * @cfg_syn_all: synchronize all configuration table.
+ * @cfg_syn: synchronize configuration table by tid.
  * @tdev_support_attr: Check whether the UMMU device supports the tdev attribute.
  */
 struct ummu_core_ops {
@@ -159,6 +172,9 @@ struct ummu_core_ops {
 		       enum eid_type type);
 	void (*del_eid)(struct ummu_core_device *dev, guid_t *guid, eid_t eid,
 			enum eid_type type);
+	int (*invalidate_cfg)(struct ummu_base_domain *d);
+	void (*cfg_sync_all)(struct ummu_base_domain *d);
+	void (*cfg_sync)(struct ummu_base_domain *d);
 	bool (*tdev_support_attr)(struct ummu_core_device *dev, struct tdev_attr *attr);
 
 	KABI_RESERVE(1)
@@ -233,6 +249,52 @@ struct ummu_tid_manager {
 	KABI_RESERVE(2)
 	KABI_RESERVE(3)
 	KABI_RESERVE(4)
+};
+
+struct ummu_core_tid_args {
+	const struct tid_ops *tid_ops;
+	u32 max_tid;
+	u32 min_tid;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
+	KABI_RESERVE(5)
+	KABI_RESERVE(6)
+};
+
+struct ummu_core_init_args {
+	const struct ummu_core_ops *core_ops;
+	struct ummu_core_tid_args tid_args;
+	const struct iommu_ops *iommu_ops;
+	struct device *hwdev;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+};
+
+/* Memory traffic monitoring of the UB device */
+struct ummu_mpam {
+#define UMMU_DEV_SET_MPAM	(1 << 0)
+#define UMMU_DEV_GET_MPAM	(1 << 1)
+#define UMMU_DEV_SET_USER_MPAM_EN	(1 << 2)
+#define UMMU_DEV_GET_USER_MPAM_EN	(1 << 3)
+	int flags;
+	eid_t eid;
+	int tid;
+	int partid;
+	int pmg;
+	int s1mpam;
+	int user_mpam_en;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+};
+
+enum ummu_device_config_type {
+	UMMU_MPAM = 0,
 };
 
 #if IS_ENABLED(CONFIG_UB_UMMU_CORE_DRIVER)
@@ -357,6 +419,162 @@ static inline int ummu_drain_pages(struct iova_slot *slot, dma_addr_t iova,
 #endif /* CONFIG_UB_UMMU_CORE */
 
 #if IS_ENABLED(CONFIG_UB_UMMU_CORE_DRIVER)
+/* UMMU SVA API */
+/**
+ * Grant va range permission to sva.
+ * @sva: related sva handle.
+ * @va: va start
+ * @size: va size
+ * @perm: permission
+ * @cookie: struct ummu_token_info*
+ *		if (!cookie) {
+ *			do not use cookie check.
+ *		} else if (cookie->input == 0) {
+ *			use this cookie->tokenval
+ *		} else if (cookie->input == 1) {
+ *			cookie->tokenval = generate new one
+ *		} else {
+ *			invalid para
+ *		}
+ *
+ * Return: 0 on success, or an error.
+ */
+int ummu_sva_grant_range(struct iommu_sva *sva, void *va, size_t size, int perm,
+			 void *cookie);
+
+/**
+ * Ungrant va range permission from sva.
+ * @sva: related sva handle.
+ * @va: va start
+ * @size: va size
+ * @cookie: va related cookie,struct ummu_token_info*
+ *		if (!cookie) {
+ *			do not use cookie check.
+ *		} else {
+ *			ungrant by cookie->tokenval
+ *		}
+ *
+ * Return: 0 an success, or an error.
+ */
+int ummu_sva_ungrant_range(struct iommu_sva *sva, void *va, size_t size,
+			   void *cookie);
+
+/**
+ * Get tid from dev or sva.
+ * @dev: related device.
+ * @sva: if sva is set, return sva mode related tid; otherwise
+ *	 return the dma mode tid.
+ * @tidp: tid returned here.
+ *
+ * Return: 0 on success, or an error.
+ */
+int ummu_get_tid(struct device *dev, struct iommu_sva *sva, u32 *tidp);
+
+/**
+ * Get iommu_domain by tid and dev.
+ * @dev: related device.
+ * @tid: tid
+ *
+ * Return: iommu_domain or NULL if failed.
+ */
+struct iommu_domain *ummu_core_get_domain_by_tid(struct device *dev,
+						 u32 tid);
+
+/**
+ * Check whether the UMMU works in ksva mode.
+ * @domain: related iommu domain
+ *
+ * Return: true or false.
+ */
+bool ummu_is_ksva(struct iommu_domain *domain);
+
+/**
+ * Check whether the UMMU works in sva mode.
+ * @domain: related iommu domain
+ *
+ * Return: true or false.
+ */
+bool ummu_is_sva(struct iommu_domain *domain);
+
+/**
+ * Bind device to a process mm.
+ * @dev: related device.
+ * @mm: process memory management.
+ * @drvdata: ummu_param related to tid.
+ *		if (!drvdata) {
+ *			sva is in the bypass mapt mode.
+ *		} else {
+ *			follow the drvdata->mode to set mapt mode.
+ *		}
+ *
+ * Return: sva handle or NULL if failed.
+ */
+struct iommu_sva *ummu_sva_bind_device(struct device *dev, struct mm_struct *mm,
+				       struct ummu_param *drvdata);
+
+/**
+ * Bind device to kernel mm.
+ * @dev: related device.
+ * @drvdata: ummu_param related to tid. ksva doesn't support bypass mapt.
+ *
+ * Return: sva handle or NULL if failed.
+ */
+struct iommu_sva *ummu_ksva_bind_device(struct device *dev,
+					struct ummu_param *drvdata);
+void ummu_sva_unbind_device(struct iommu_sva *handle);
+void ummu_ksva_unbind_device(struct iommu_sva *handle);
+
+/* UMMU CORE API */
+/**
+ * Initialiase ummu core device.
+ * @ummu_core: ummu core device.
+ * @args: ummu core init args.
+ * UMMU driver should carefully choose the args based on its requirement.
+ *	iommu_ops is mandatory.
+ *	a. the ummu device need tid allocation capability.
+ *		a.1 default tid strategies satisfy the ummu device
+ *			-> set tid_ops form ummu_core_tid_ops[TID_OPS_MAX]
+ *		a.2 default tid strategies do not satisfy the ummu device
+ *			-> implement a new tid_ops in the driver.
+ *	b. the ummu device need ummu core ops capability.
+ *		-> set core_ops.
+ *	c. the ummu device has related hwdev.
+ *		-> set hwdev.
+ */
+int ummu_core_device_init(struct ummu_core_device *ummu_core,
+			  struct ummu_core_init_args *args);
+/**
+ * Deinitialiase ummu core device.
+ * @ummu_core: ummu core device.
+ */
+void ummu_core_device_deinit(struct ummu_core_device *ummu_core);
+
+/**
+ * Register ummu core device to the ummu framework.
+ * @ummu_core: ummu core device.
+ * @type: register type.
+	REGISTER_TYPE_GLOBAL: register the ummu device as the global device,
+		The ummu device will be the device handle all request.
+		e.g. 1. add_eid/del_eid 2. provide ubus iommu ops. etc.
+
+	REGISTER_TYPE_NORMAL: follow the iommu_device register. will not be
+		related to the global device. it work as a normal iommu device.
+ */
+int ummu_core_device_register(struct ummu_core_device *ummu_core,
+			      enum ummu_register_type type);
+/**
+ * Unregister ummu core device from the ummu framework.
+ * @dev: the ummu_core device tid belongs to.
+ */
+void ummu_core_device_unregister(struct ummu_core_device *dev);
+
+/**
+ * Invalidate ummu global configuration by tid.
+ * @tid: tid
+ * Return: 0 on success, or an error.
+ */
+int ummu_core_invalidate_cfg_table(u32 tid);
+
 /* UMMU TID API */
 /**
  * Alloc a tid from ummu framework, and alloc related pasid.
@@ -413,6 +631,82 @@ struct device *ummu_core_alloc_tdev(struct tdev_attr *attr, u32 *ptid);
  */
 int ummu_core_free_tdev(struct device *dev);
 #else
+static inline int ummu_sva_grant_range(struct iommu_sva *sva, void *va,
+				       size_t size, int perm, void *cookie)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int ummu_sva_ungrant_range(struct iommu_sva *sva, void *va,
+					 size_t size, void *cookie)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int ummu_get_tid(struct device *dev, struct iommu_sva *sva,
+			       u32 *tidp)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline struct iommu_domain *
+ummu_core_get_domain_by_tid(struct device *dev, u32 tid)
+{
+	return NULL;
+}
+
+static inline bool ummu_is_ksva(struct iommu_domain *domain)
+{
+	return false;
+}
+
+static inline bool ummu_is_sva(struct iommu_domain *domain)
+{
+	return false;
+}
+
+static inline struct iommu_sva *ummu_sva_bind_device(struct device *dev,
+						     struct mm_struct *mm,
+						     struct ummu_param *drvdata)
+{
+	return NULL;
+}
+
+static inline struct iommu_sva *
+ummu_ksva_bind_device(struct device *dev, struct ummu_param *drvdata)
+{
+	return NULL;
+}
+
+static inline void ummu_sva_unbind_device(struct iommu_sva *handle)
+{
+}
+static inline void ummu_ksva_unbind_device(struct iommu_sva *handle)
+{
+}
+static inline int ummu_core_device_init(struct ummu_core_device *ummu_core,
+					struct ummu_core_init_args *args)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void ummu_core_device_deinit(struct ummu_core_device *ummu_core)
+{
+}
+static inline int ummu_core_device_register(struct ummu_core_device *ummu_core,
+					    enum ummu_register_type type)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void ummu_core_device_unregister(struct ummu_core_device *dev)
+{
+}
+static inline int ummu_core_invalidate_cfg_table(u32 tid)
+{
+	return -EOPNOTSUPP;
+}
+
 static inline int ummu_core_alloc_tid(struct ummu_core_device *dev,
 				      struct ummu_tid_param *drvdata,
 				      u32 *tidp)

kunpengcompute/ub-os-component-dkms

commit b4c9017279e6edebe7fd21902fbe16ddf24ac2f2

iommu/ummu-core: fix uaf in ummu_core_invalidate_cfg()
ubmmu inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9073

(cherry-pick from OLK-6.6 commit 53658de93113)

--------------------------------

In the user-space udma module, during the invalidation of a tid prior
to its deletion, an address access failure is encountered at address
0xdeacfffffffffff8 when the base_domain member is accessed within the
ummu_cfg_sync function.

This issue arises during process termination when the so
(shared object) is being destructed, leading to the removal of all tid
and domain resources. If a thread calls urma_unregister_seg during
this period, it triggers the udma invalidation process, which
unfortunately attempts to access memory that has already been freed,
causing the failure.

To solve this problem, the key of the proc_info_xa process is changed to
the mm of the process, so that the process lock can be obtained during
invalidation to be mutually exclusive with the deletion of the tid.

Fixes: 9db5eb9c30ff ("iommu/ummu-core: core interfaces for ummu drivers")

Signed-off-by: Liming An <anliming1@h-partners.com>
Signed-off-by: Wang Hai <wanghai38@huawei.com>

drivers/iommu/hisilicon/ummu-core/core.c
@@ -470,52 +470,11 @@ EXPORT_SYMBOL_GPL(ummu_core_invalidate_cfg_table);
 
 int ummu_core_invalidate_cfg(struct ummu_invalid_cfg_param *param)
 {
-	struct iommu_domain *domain;
-	struct mm_struct *tid_mm;
-	struct device *dev;
-	int ret;
 
-	if (!param)
+	if (!param || !param->mm)
 		return -EINVAL;
 
-	if (!param->mm)
-		return -EFAULT;
-
-	mutex_lock(&global_device_lock);
-	if (!global_core_device) {
-		ret = -ENOENT;
-		goto out_unlock;
-	}
-
-	tid_mm = ummu_core_get_mm(global_core_device, param->tid);
-	if (param->mm != tid_mm) {
-		ret = -ESPIPE;
-		goto out_unlock;
-	}
-
-	dev = ummu_core_get_device(global_core_device, param->tid);
-	if (!dev) {
-		ret = -ENODEV;
-		goto out_unlock;
-	}
-
-	domain = ummu_core_get_domain_by_tid(dev, param->tid);
-	if (!domain) {
-		ret = -ENXIO;
-		goto out_put_device;
-	}
-
-	if (!global_core_device->ops || !global_core_device->ops->invalidate_cfg) {
-		ret = -EOPNOTSUPP;
-		goto out_put_device;
-	}
-	ret = global_core_device->ops->invalidate_cfg(to_ummu_base_domain(domain));
-
-out_put_device:
-	ummu_core_put_device(dev);
-out_unlock:
-	mutex_unlock(&global_device_lock);
-	return ret;
+	return ummu_core_invalidate_tid(param->mm, param->tid);
 }
 EXPORT_SYMBOL_GPL(ummu_core_invalidate_cfg);
 

drivers/iommu/hisilicon/ummu-core/core_ioctl.c
@@ -23,10 +23,6 @@
 #define MMAP_BLOCK_INDEX_MASK GENMASK(11, 2)
 #define MMAP_BLK_TID_MASK GENMASK(31, 12)
 #define MMAP_QUE_TID_MASK GENMASK(21, 2)
-#define KEY_TIME_REGIN GENMASK_ULL(47, 16)
-#define KEY_OFFSET_SHIFT 32
-#define GET_PROC_KEY(tgid, stime) ((((u64)(tgid)) << (KEY_OFFSET_SHIFT)) \
-			| ((u64)(FIELD_GET(KEY_TIME_REGIN, stime))))
 
 struct ktid_info {
 	u8 pcmdq_order;
@@ -93,13 +89,12 @@ static int get_tid_res(struct ktid_info *entry)
 
 static int proc_manager_open(struct inode *inode, struct file *filp)
 {
+	struct mm_struct *mm = current->mm;
 	struct proc_manager *manager;
-	unsigned long key;
 	int ret;
 
 	mutex_lock(&global_proc_mtx);
-	key = GET_PROC_KEY(current->tgid, current->start_time);
-	manager = xa_load(&proc_info_xa, key);
+	manager = xa_load(&proc_info_xa, (unsigned long)mm);
 	if (manager) {
 		mutex_unlock(&global_proc_mtx);
 		return -EEXIST;
@@ -113,14 +108,15 @@ static int proc_manager_open(struct inode *inode, struct file *filp)
 
 	mutex_init(&manager->proc_mtx);
 	mutex_lock(&manager->proc_mtx);
-	ret = xa_err(xa_store(&proc_info_xa, key, manager, GFP_KERNEL));
+	ret = xa_err(xa_store(&proc_info_xa, (unsigned long)mm, manager, GFP_KERNEL));
 	if (ret)
 		goto mtx_unlock;
 
 	mutex_unlock(&global_proc_mtx);
+	mmgrab(mm);
 
 	xa_init(&manager->tid_xa);
-	filp->private_data = (void *)(uintptr_t)key;
+	filp->private_data = mm;
 	mutex_unlock(&manager->proc_mtx);
 
 	return 0;
@@ -170,21 +166,22 @@ static void release_tid_resource(struct ktid_info *entry)
 
 static int proc_manager_close(struct inode *inode, struct file *filp)
 {
-	unsigned long key = (unsigned long)(uintptr_t)filp->private_data;
+	struct mm_struct *mm = filp->private_data;
 	struct proc_manager *manager;
 	struct ktid_info *entry;
 	unsigned long idx = 0;
 
 	mutex_lock(&global_proc_mtx);
-	manager = xa_load(&proc_info_xa, key);
+	manager = xa_load(&proc_info_xa, (unsigned long)mm);
 	if (!manager) {
 		mutex_unlock(&global_proc_mtx);
 		return -EINVAL;
 	}
 
-	WARN_ON(!xa_erase(&proc_info_xa, key));
+	xa_erase(&proc_info_xa, (unsigned long)mm);
 	mutex_lock(&manager->proc_mtx);
 	mutex_unlock(&global_proc_mtx);
+	mmdrop(mm);
 	if (!xa_empty(&manager->tid_xa))
 		xa_for_each(&manager->tid_xa, idx, entry) {
 			release_tid_resource(entry);
@@ -865,6 +862,40 @@ static long tid_ioctl(struct file *filp, u32 cmd, unsigned long arg)
 		ret = alloc_tid_response(&tid_data, key, entry, arg);
 
 	return ret;
+}
+
+int ummu_core_invalidate_tid(struct mm_struct *mm, u32 tid)
+{
+	struct proc_manager *manager;
+	struct iommu_domain *domain;
+	struct ktid_info *entry;
+
+	mutex_lock(&global_proc_mtx);
+	manager = xa_load(&proc_info_xa, (unsigned long)mm);
+	if (!manager) {
+		mutex_unlock(&global_proc_mtx);
+		return -ESRCH;
+	}
+
+	guard(mutex)(&manager->proc_mtx);
+	mutex_unlock(&global_proc_mtx);
+
+	entry = xa_load(&manager->tid_xa, tid);
+	if (!entry)
+		return -ENOENT;
+
+	if (entry->sva)
+		domain = entry->sva->handle.domain;
+	else
+		domain = iommu_get_domain_for_dev(entry->dev);
+
+	if (!domain)
+		return -ENODEV;
+
+	if (!global_core_device->ops || !global_core_device->ops->invalidate_cfg)
+		return -EOPNOTSUPP;
+
+	return global_core_device->ops->invalidate_cfg(to_ummu_base_domain(domain));
 }
 
 static const struct file_operations misc_fops = {

drivers/iommu/hisilicon/ummu-core/ummu_core_priv.h
@@ -42,5 +42,6 @@ void ummu_core_put_resource(struct iommu_sva *sva, struct device *dev,
 int ummu_core_get_hw_cap(struct device *dev, u32 *hw_cap);
 struct mm_struct *ummu_core_get_mm(struct ummu_core_device *ummu_core, u32 tid);
 void setup_tdev_dma_ops(struct device *dev, bool coherent);
+int ummu_core_invalidate_tid(struct mm_struct *mm, u32 tid);
 
 #endif /* __UMMU_CORE_PRIV_H__ */