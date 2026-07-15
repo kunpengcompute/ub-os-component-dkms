OERV-RVCI/rvck-olk

commit f79bb14f3d282c86296a285ab573a8d990ba56fd

ub: ubase&unic: adjust the encoding format of the ubase&unic driver.
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/8760
CVE: NA

----------------------------------------------------------

This patch mainly modifies the following:
1. Unified the header file format and added comments at the end.
2. Adjusted the code format, such as line breaks and alignment, to
improve code readability.
3. Deleted the void cast when the return value is not used, in
compliance with the open source coding specifications.
4. Corrected the incorrect symbols in code comments.
5. Changed the kzalloc function to the kcalloc function to improve
the security of kernel memory allocation.

Fixes: a1f4613490f8 ("net: unic: add support of unic driver for ub network")
Signed-off-by: Xiongchuan Zhou <zhouxiongchuan@h-partners.com>

drivers/net/ub/unic/debugfs/unic_debugfs.c
@@ -98,22 +98,22 @@ static const struct unic_dbg_cap_bit_info {
 	const char *format;
 	bool (*get_bit)(struct unic_dev *dev);
 } unic_cap_bits[] = {
-	{"\tsupport_ubl: %u\n", &unic_dev_ubl_supported},
-	{"\tsupport_pfc: %u\n", &unic_dev_pfc_supported},
-	{"\tsupport_ets: %u\n", &unic_dev_ets_supported},
-	{"\tsupport_fec: %u\n", &unic_dev_fec_supported},
-	{"\tsupport_pause: %u\n", &unic_dev_pause_supported},
-	{"\tsupport_eth: %u\n", &unic_dev_eth_supported},
-	{"\tsupport_tc_speed_limit: %u\n", &unic_dev_tc_speed_limit_supported},
-	{"\tsupport_tx_csum_offload: %u\n", &unic_dev_tx_csum_offload_supported},
-	{"\tsupport_rx_csum_offload: %u\n", &unic_dev_rx_csum_offload_supported},
-	{"\tsupport_app_lb: %u\n", &unic_dev_app_lb_supported},
-	{"\tsupport_external_lb: %u\n", &unic_dev_external_lb_supported},
-	{"\tsupport_serial_serdes_lb: %u\n", &unic_dev_serial_serdes_lb_supported},
-	{"\tsupport_parallel_serdes_lb: %u\n", &unic_dev_parallel_serdes_lb_supported},
-	{"\tsupport_fec_stats: %u\n", &unic_dev_fec_stats_supported},
-	{"\tsupport_cfg_mac: %u\n", &unic_dev_cfg_mac_supported},
-	{"\tsupport_cfg_vlan_filter: %u\n", &unic_dev_cfg_vlan_filter_supported},
+	{"\tsupport_ubl: %u\n", unic_dev_ubl_supported},
+	{"\tsupport_pfc: %u\n", unic_dev_pfc_supported},
+	{"\tsupport_ets: %u\n", unic_dev_ets_supported},
+	{"\tsupport_fec: %u\n", unic_dev_fec_supported},
+	{"\tsupport_pause: %u\n", unic_dev_pause_supported},
+	{"\tsupport_eth: %u\n", unic_dev_eth_supported},
+	{"\tsupport_tc_speed_limit: %u\n", unic_dev_tc_speed_limit_supported},
+	{"\tsupport_tx_csum_offload: %u\n", unic_dev_tx_csum_offload_supported},
+	{"\tsupport_rx_csum_offload: %u\n", unic_dev_rx_csum_offload_supported},
+	{"\tsupport_app_lb: %u\n", unic_dev_app_lb_supported},
+	{"\tsupport_external_lb: %u\n", unic_dev_external_lb_supported},
+	{"\tsupport_serial_serdes_lb: %u\n", unic_dev_serial_serdes_lb_supported},
+	{"\tsupport_parallel_serdes_lb: %u\n", unic_dev_parallel_serdes_lb_supported},
+	{"\tsupport_fec_stats: %u\n", unic_dev_fec_stats_supported},
+	{"\tsupport_cfg_mac: %u\n", unic_dev_cfg_mac_supported},
+	{"\tsupport_cfg_vlan_filter: %u\n", unic_dev_cfg_vlan_filter_supported},
 };
 
 static void unic_dbg_dump_caps_bits(struct unic_dev *unic_dev,
@@ -583,7 +583,7 @@ static struct ubase_dbg_cmd_info unic_dbg_cmd[] = {
 		.support = unic_dbg_dentry_support,
 		.init = ubase_dbg_seq_file_init,
 		.read_func = unic_dbg_clear_link_record,
-	}
+	},
 };
 
 int unic_dbg_init(struct auxiliary_device *adev)

drivers/net/ub/unic/unic_bond.c
@@ -238,7 +238,8 @@ static int unic_send_bond_status_change_notify(struct auxiliary_device *adev,
 
 	ret = ubase_ctrlq_send_msg(adev, &msg);
 	if (ret)
-		unic_err(unic_dev, "failed to notify bond status change, port id = %u, bonding_cmd = %s, ret = %d.\n",
+		unic_err(unic_dev,
+			 "failed to notify bond status change, port id = %u, bonding_cmd = %s, ret = %d.\n",
 			 req.port_id, req.bonding_cmd == UNIC_CTRLQ_BOND_ADD_PORT ?
 			 "ADD" : "DEL", ret);
 

drivers/net/ub/unic/unic_lb.c
@@ -168,7 +168,7 @@ static int unic_lb_config(struct net_device *ndev, int loop_type, bool en)
 			 "lb_config return error, ret = %d, enable = %d.\n",
 			 ret, en);
 
-	(void)unic_set_promisc_mode(unic_dev, en ? &promisc_all_en : &promisc_en);
+	unic_set_promisc_mode(unic_dev, en ? &promisc_all_en : &promisc_en);
 
 	return ret;
 }
@@ -573,7 +573,7 @@ void unic_self_test(struct net_device *ndev,
 	int ret, i;
 
 	ret = unic_get_selftest_count(unic_dev);
-	if (ret <= 0) {
+	if (ret == -EOPNOTSUPP) {
 		eth_test->flags |= ETH_TEST_FL_FAILED;
 		return;
 	}

drivers/net/ub/unic/unic_netdev.c
@@ -509,7 +509,8 @@ static int unic_change_mtu(struct net_device *netdev, int new_mtu)
 	int ret;
 
 	if (netif_running(netdev)) {
-		unic_err(unic_dev, "failed to change MTU, due to network interface is up, please down it first and try again.\n");
+		unic_err(unic_dev,
+			 "failed to change MTU, due to network interface is up, please down it first and try again.\n");
 		return -EBUSY;
 	}
 

kunpengcompute/ub-os-component-dkms

commit 38755ae40017e8aa1aecc1c3b43374c5f3932a7b

﻿net: unic: Log rectification for the wildfire project
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9296
CVE: NA

(cherry-pick from OLK-6.6 commit 2e32447343ef)

--------------------------------------------------------------------------

Delete log information containing sensitive words, correct word spellings,
and improve the readability of the logs.

Fixes: f79bb14f3d28 ("ub: ubase&unic: adjust the encoding format of the ubase&unic driver")
Signed-off-by: Xu Wang <wangxu341@h-partners.com>
Signed-off-by: Wang Hai <wanghai38@huawei.com>

drivers/net/ub/unic/debugfs/unic_debugfs.c
@@ -744,7 +744,7 @@ int unic_dbg_init(struct auxiliary_device *adev)
 	unic_dev = (struct unic_dev *)dev_get_drvdata(dev);
 
 	if (!ubase_root_dentry) {
-		unic_err(unic_dev, "dbgfs root dentry does not exist.\n");
+		unic_err(unic_dev, "debugfs root dentry does not exist.\n");
 		return -ENOENT;
 	}
 

drivers/net/ub/unic/unic_bond.c
@@ -264,9 +264,9 @@ int unic_sync_bond_status(struct net_device *netdev)
 		ret = unic_send_bond_status_change_notify(unic_dev->comdev.adev,
 							  bond_cmd);
 		if (!ret) {
-			unic_info(unic_dev,
-				  "update last_notify_status: %u -> %u.\n",
-				  bond_status->last_notify_status, cur_status);
+			unic_dbg(unic_dev,
+				 "update last_notify_status: %u -> %u.\n",
+				 bond_status->last_notify_status, cur_status);
 			bond_status->last_notify_status = cur_status;
 		}
 	}

drivers/net/ub/unic/unic_lb.c
@@ -165,7 +165,7 @@ static int unic_lb_config(struct net_device *ndev, int loop_type, bool en)
 
 	if (ret && ret != -EOPNOTSUPP)
 		unic_err(unic_dev,
-			 "lb_config return error, ret = %d, enable = %d.\n",
+			 "loopback_config return error, ret = %d, enable = %d.\n",
 			 ret, en);
 
 	unic_set_promisc_mode(unic_dev, en ? &promisc_all_en : &promisc_en);

drivers/net/ub/unic/unic_netdev.c
@@ -939,8 +939,8 @@ static int unic_update_bond_status(struct unic_dev *unic_dev,
 	unic_dev->bond_status.cur_status = lag_info->tx_enabled;
 	mutex_unlock(&bond_status->mutex);
 
-	unic_info(unic_dev, "[UNIC DEBUG] update bond_status: %u.\n",
-		  unic_dev->bond_status.cur_status);
+	unic_dbg(unic_dev,
+		 "update bond_status: %u.\n", unic_dev->bond_status.cur_status);
 
 	unic_bond_status_record(unic_dev->comdev.netdev, lag_info->tx_enabled);
 