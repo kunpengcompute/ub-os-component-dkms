chenglongshe/openEuler

ub:ubfi: Support ub firmware interface basic functions
driver inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/ID34DG
CVE: NA

-----------------------------------------------------------

Support UnifiedBus firmware interface basic functions,
set bios mode, get ub root table by dts or acpi,
handle dts-ubrt or acpi-ubrt

Signed-off-by: Yuhao Xiang <xiangyuhao2@huawei.com>

drivers/ub/ubfi/ub_fi.c
@@ -1,0 +1,127 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubfi driver: " fmt
+
+#include <linux/acpi.h>
+#include <linux/module.h>
+#include <linux/of.h>
+
+#include "ubrt.h"
+#include "ub_fi.h"
+
+#define ACPI_SIG_UBRT "UBRT" /* UB Root Table */
+#define UBIOS_INFO_TABLE "linux,ubios-information-table"
+
+enum bios_report_mode bios_mode = UNKNOWN;
+
+static void ub_bios_mode_init(void)
+{
+	if (acpi_disabled)
+		bios_mode = DTS;
+	else
+		bios_mode = ACPI;
+
+	pr_info("Starting with mode: %d\n", bios_mode);
+}
+
+static int ubfi_get_acpi_ubrt(void)
+{
+	struct acpi_table_header *header;
+	acpi_status status;
+
+	status = acpi_get_table(ACPI_SIG_UBRT, 0, &header);
+	if (ACPI_FAILURE(status)) {
+		pr_err("ACPI failed to get UBRT.\n");
+		if (status != AE_NOT_FOUND)
+			pr_err("ACPI failed msg: %s\n",
+				acpi_format_exception(status));
+		return -ENODEV;
+	}
+	acpi_table = (struct acpi_table_ubrt *)header;
+	pr_info("get ubrt by acpi success\n");
+	return 0;
+}
+
+static int ubfi_get_dts_ubrt(void)
+{
+	struct device_node *node;
+	u64 phys_addr;
+
+	node = of_find_node_by_path("/chosen");
+	if (!node) {
+		pr_err("Failed to get device tree chosen node\n");
+		return -EINVAL;
+	}
+
+	if (of_property_read_u64(node, UBIOS_INFO_TABLE, &phys_addr)) {
+		pr_err("Failed to get %s node\n", UBIOS_INFO_TABLE);
+		return -EINVAL;
+	}
+
+	ubios_table = (struct ubios_root_table *)ub_table_get(phys_addr);
+	if (!ubios_table)
+		return -ENOMEM;
+
+	pr_info("ubfi get ubrt by device tree success\n");
+	return 0;
+}
+
+static int ubfi_get_ubrt(void)
+{
+	if (bios_mode == ACPI)
+		return ubfi_get_acpi_ubrt();
+	else if (bios_mode == DTS)
+		return ubfi_get_dts_ubrt();
+	return -EINVAL;
+}
+
+static int handle_ubrt(void)
+{
+	if (bios_mode == ACPI)
+		return handle_acpi_ubrt();
+	else if (bios_mode == DTS)
+		return handle_dts_ubrt();
+
+	return -EINVAL;
+}
+
+static void ubfi_put_ubrt(void)
+{
+	if (bios_mode == ACPI) {
+		acpi_put_table((struct acpi_table_header *)acpi_table);
+		acpi_table = NULL;
+	} else if (bios_mode == DTS) {
+		ub_table_put(ubios_table);
+		ubios_table = NULL;
+	}
+}
+
+static int __init ubfi_init(void)
+{
+	int ret;
+
+	ub_bios_mode_init();
+
+	ret = ubfi_get_ubrt();
+	if (ret) {
+		pr_info("can't get ub information from bios, ret=%d\n", ret);
+		return 0;
+	}
+
+	return handle_ubrt();
+}
+
+static void __exit ubfi_exit(void)
+{
+	uninit_ub_nodes();
+	ubfi_put_ubrt();
+}
+
+module_init(ubfi_init);
+module_exit(ubfi_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("UnifiedBus firmware interface driver");
+MODULE_IMPORT_NS(UB_UBFI);

drivers/ub/ubfi/ubrt.c
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubfi ubrt: " fmt
+
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+
+#include "ubrt.h"
+
+#define UBIOS_SIG_UBC "ubc"
+
+struct acpi_table_ubrt *acpi_table;
+struct ubios_root_table *ubios_table;
+
+/*
+ * ummu max count is 32, max size is 40 + 32 * 128 = 4640
+ * ubc max count is 32, max size is 40 + 88 + 32 * 256 + 32 * 4 = 8448
+ */
+#define UBIOS_TABLE_TOTLE_SIZE_MAX 8448
+
+/* remember to use ub_table_put to release memory alloced by ub_table_get */
+void *ub_table_get(u64 pa)
+{
+	void __iomem *va;
+	u32 total_size;
+	void *ret;
+
+	if (!pa)
+		return NULL;
+
+	va = ioremap(pa, sizeof(struct ub_table_header));
+	if (!va) {
+		pr_err("ioremap ub table header fail\n");
+		return NULL;
+	}
+
+	total_size = readl(va + UB_TABLE_HEADER_NAME_LEN);
+	pr_debug("ub table size is[0x%x]\n", total_size);
+	if (total_size == 0 || total_size > UBIOS_TABLE_TOTLE_SIZE_MAX) {
+		pr_err("ubios table size is invalid\n");
+		iounmap(va);
+		return NULL;
+	}
+	iounmap(va);
+
+	va = ioremap(pa, total_size);
+	if (!va) {
+		pr_err("ioremap full ub table fail\n");
+		return NULL;
+	}
+
+	ret = kzalloc(total_size, GFP_KERNEL);
+	if (!ret) {
+		iounmap(va);
+		return NULL;
+	}
+
+	memcpy_fromio(ret, va, total_size);
+	iounmap(va);
+	return ret;
+}
+
+void ub_table_put(void *va)
+{
+	kfree(va);
+}
+
+void uninit_ub_nodes(void)
+{
+}
+
+int handle_acpi_ubrt(void)
+{
+	struct ubrt_sub_table *sub_table;
+	int ret = 0;
+	u32 i;
+
+	pr_info("acpi ubrt sub table count is %u\n", acpi_table->count);
+
+	for (i = 0; i < acpi_table->count; i++) {
+		sub_table = &acpi_table->sub_table[i];
+		switch (sub_table->type) {
+		case UB_BUS_CONTROLLER_TABLE:
+			break;
+		default:
+			pr_warn("Ignore sub table: type %u\n", sub_table->type);
+			break;
+		}
+		if (ret) {
+			pr_err("parse ubrt sub table type %u fail\n",
+				sub_table->type);
+			goto fail;
+		}
+	}
+	return ret;
+fail:
+	uninit_ub_nodes();
+	return ret;
+}
+
+int handle_dts_ubrt(void)
+{
+	char name[UB_TABLE_HEADER_NAME_LEN] = {};
+	struct ub_table_header *header;
+	int ret = 0, i;
+
+	if (ubios_table->count == 0) {
+		pr_err("ubios root table has no sub tables.\n");
+		return 0;
+	}
+	pr_info("ubios sub table count is %u\n", ubios_table->count);
+
+	for (i = 0; i < ubios_table->count; i++) {
+		header = (struct ub_table_header *)ub_table_get(
+			 ubios_table->sub_tables[i]);
+		if (!header)
+			continue;
+
+		memcpy(name, header->name, UB_TABLE_HEADER_NAME_LEN - 1);
+		pr_info("ubrt sub table name is %s\n", name);
+		ub_table_put(header);
+
+		if (!strncmp(name, UBIOS_SIG_UBC, strlen(UBIOS_SIG_UBC)))
+			ret = 0;
+		else
+			pr_warn("Ignore sub table: %s\n", name);
+
+		if (ret) {
+			pr_err("Create %s device ret=%d\n", name, ret);
+			goto out;
+		}
+	}
+
+	return 0;
+
+out:
+	uninit_ub_nodes();
+	return ret;
+}

kunpengcompute/ub-os-component-dkms

drivers/ub/ubfi/ub_fi.c
@@ -1,0 +1,127 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubfi driver: " fmt
+
+#include <linux/acpi.h>
+#include <linux/module.h>
+#include <linux/of.h>
+
+#include "ubrt.h"
+#include "ub_fi.h"
+
+#define ACPI_SIG_UBRT "UBRT" /* UB Root Table */
+#define UBIOS_INFO_TABLE "linux,ubios-information-table"
+
+enum bios_report_mode bios_mode = UNKNOWN;
+
+static void ub_bios_mode_init(void)
+{
+	if (acpi_disabled)
+		bios_mode = DTS;
+	else
+		bios_mode = ACPI;
+
+	pr_info("Starting with mode: %d\n", bios_mode);
+}
+
+static int ubfi_get_acpi_ubrt(void)
+{
+	struct acpi_table_header *header;
+	acpi_status status;
+
+	status = acpi_get_table(ACPI_SIG_UBRT, 0, &header);
+	if (ACPI_FAILURE(status)) {
+		pr_err("ACPI failed to get UBRT.\n");
+		if (status != AE_NOT_FOUND)
+			pr_err("ACPI failed msg: %s\n",
+				acpi_format_exception(status));
+		return -ENODEV;
+	}
+	acpi_table = (struct acpi_table_ubrt *)header;
+	pr_info("get ubrt by acpi success\n");
+	return 0;
+}
+
+static int ubfi_get_dts_ubrt(void)
+{
+	struct device_node *node;
+	u64 phys_addr;
+
+	node = of_find_node_by_path("/chosen");
+	if (!node) {
+		pr_err("Failed to get device tree chosen node\n");
+		return -EINVAL;
+	}
+
+	if (of_property_read_u64(node, UBIOS_INFO_TABLE, &phys_addr)) {
+		pr_err("Failed to get %s node\n", UBIOS_INFO_TABLE);
+		return -EINVAL;
+	}
+
+	ubios_table = (struct ubios_root_table *)ub_table_get(phys_addr);
+	if (!ubios_table)
+		return -ENOMEM;
+
+	pr_info("ubfi get ubrt by device tree success\n");
+	return 0;
+}
+
+static int ubfi_get_ubrt(void)
+{
+	if (bios_mode == ACPI)
+		return ubfi_get_acpi_ubrt();
+	else if (bios_mode == DTS)
+		return ubfi_get_dts_ubrt();
+	return -EINVAL;
+}
+
+static int handle_ubrt(void)
+{
+	if (bios_mode == ACPI)
+		return handle_acpi_ubrt();
+	else if (bios_mode == DTS)
+		return handle_dts_ubrt();
+
+	return -EINVAL;
+}
+
+static void ubfi_put_ubrt(void)
+{
+	if (bios_mode == ACPI) {
+		acpi_put_table((struct acpi_table_header *)acpi_table);
+		acpi_table = NULL;
+	} else if (bios_mode == DTS) {
+		ub_table_put(ubios_table);
+		ubios_table = NULL;
+	}
+}
+
+static int __init ubfi_init(void)
+{
+	int ret;
+
+	ub_bios_mode_init();
+
+	ret = ubfi_get_ubrt();
+	if (ret) {
+		pr_warn("can't get ub information from bios, ret=%d\n", ret);
+		return 0;
+	}
+
+	return handle_ubrt();
+}
+
+static void __exit ubfi_exit(void)
+{
+	uninit_ub_nodes();
+	ubfi_put_ubrt();
+}
+
+module_init(ubfi_init);
+module_exit(ubfi_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("UnifiedBus firmware interface driver");
+MODULE_IMPORT_NS(UB_UBFI);

drivers/ub/ubfi/ubrt.c
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) HiSilicon Technologies Co., Ltd. 2025. All rights reserved.
+ */
+
+#define pr_fmt(fmt)	"ubfi ubrt: " fmt
+
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+
+#include "ubrt.h"
+
+#define UBIOS_SIG_UBC "ubc"
+
+struct acpi_table_ubrt *acpi_table;
+struct ubios_root_table *ubios_table;
+
+/*
+ * ummu max count is 32, max size is 40 + 32 * 128 = 4640
+ * ubc max count is 32, max size is 40 + 88 + 32 * 256 + 32 * 4 = 8448
+ */
+#define UBIOS_TABLE_TOTLE_SIZE_MAX 8448
+
+/* remember to use ub_table_put to release memory alloced by ub_table_get */
+void *ub_table_get(u64 pa)
+{
+	void __iomem *va;
+	u32 total_size;
+	void *ret;
+
+	if (!pa)
+		return NULL;
+
+	va = ioremap(pa, sizeof(struct ub_table_header));
+	if (!va) {
+		pr_err("ioremap ub table header failed\n");
+		return NULL;
+	}
+
+	total_size = readl(va + UB_TABLE_HEADER_NAME_LEN);
+	pr_debug("ub table size is[0x%x]\n", total_size);
+	if (total_size == 0 || total_size > UBIOS_TABLE_TOTLE_SIZE_MAX) {
+		pr_err("ubios table size is invalid\n");
+		iounmap(va);
+		return NULL;
+	}
+	iounmap(va);
+
+	va = ioremap(pa, total_size);
+	if (!va) {
+		pr_err("ioremap full ub table failed\n");
+		return NULL;
+	}
+
+	ret = kzalloc(total_size, GFP_KERNEL);
+	if (!ret) {
+		iounmap(va);
+		return NULL;
+	}
+
+	memcpy_fromio(ret, va, total_size);
+	iounmap(va);
+	return ret;
+}
+
+void ub_table_put(void *va)
+{
+	kfree(va);
+}
+
+void uninit_ub_nodes(void)
+{
+}
+
+int handle_acpi_ubrt(void)
+{
+	struct ubrt_sub_table *sub_table;
+	int ret = 0;
+	u32 i;
+
+	pr_info("acpi ubrt sub table count is %u\n", acpi_table->count);
+
+	for (i = 0; i < acpi_table->count; i++) {
+		sub_table = &acpi_table->sub_table[i];
+		switch (sub_table->type) {
+		case UB_BUS_CONTROLLER_TABLE:
+			break;
+		default:
+			pr_warn("Ignore sub table: type %u\n", sub_table->type);
+			break;
+		}
+		if (ret) {
+			pr_err("parse ubrt sub table type %u failed\n",
+				sub_table->type);
+			goto fail;
+		}
+	}
+	return ret;
+fail:
+	uninit_ub_nodes();
+	return ret;
+}
+
+int handle_dts_ubrt(void)
+{
+	char name[UB_TABLE_HEADER_NAME_LEN] = {};
+	struct ub_table_header *header;
+	int ret = 0, i;
+
+	if (ubios_table->count == 0) {
+		pr_err("ubios root table has no sub tables.\n");
+		return 0;
+	}
+	pr_info("ubios sub table count is %u\n", ubios_table->count);
+
+	for (i = 0; i < ubios_table->count; i++) {
+		header = (struct ub_table_header *)ub_table_get(
+			 ubios_table->sub_tables[i]);
+		if (!header)
+			continue;
+
+		memcpy(name, header->name, UB_TABLE_HEADER_NAME_LEN - 1);
+		pr_info("ubrt sub table name is %s\n", name);
+		ub_table_put(header);
+
+		if (!strncmp(name, UBIOS_SIG_UBC, strlen(UBIOS_SIG_UBC)))
+			ret = 0;
+		else
+			pr_warn("Ignore sub table: %s\n", name);
+
+		if (ret) {
+			pr_err("Create %s device ret=%d\n", name, ret);
+			goto out;
+		}
+	}
+
+	return 0;
+
+out:
+	uninit_ub_nodes();
+	return ret;
+}