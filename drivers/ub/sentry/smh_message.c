OERV-RVCI/rvck-olk

commit 1e3e4971f8d7c3b195b43b4cb46ae00d079ba466

drivers/ub/sentry: add sentry_reporter and sentry_msg_help
drivers inclusion
category: feature
bugzilla: https://gitee.com/openeuler/release-management/issues/ID45WQ?from=project-issue

--------------------------------

Listens to kernel event(eg. oom) and provides device / api to send msg.
Support reporting the following abnormal events:
1. report oom event
2. report power off evnet
3. report ub mem err event and send SIGBUS signal to the process who accessing the faulty memory.

Signed-off-by: guodashun <guodashun1@huawei.com>
Signed-off-by: shixuantong <shixuantong1@huawei.com>

drivers/ub/sentry/smh_message.c
@@ -1,0 +1,380 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ *
+ * Description: Sentry Msg Helper
+ * Author: Luckky
+ * Create: 2025-02-17
+ */
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kfifo.h>
+#include <linux/ratelimit.h>
+
+#include "smh_message.h"
+
+static DEFINE_RATELIMIT_STATE(msg_log_rs, HZ, 10);
+
+#undef pr_fmt
+#define pr_fmt(fmt) "[sentry][message_helper]: " fmt
+
+#define RM_LOG_INFO(fmt, ...)	\
+	do {	\
+		if (__ratelimit(&msg_log_rs)) {	\
+			printk(KERN_INFO pr_fmt(fmt), ##__VA_ARGS__);	\
+		}	\
+	} while (0)
+
+
+#define RM_LOG_WARN(fmt, ...)	\
+	do {	\
+		if (__ratelimit(&msg_log_rs)) {	\
+			printk(KERN_WARNING pr_fmt(fmt), ##__VA_ARGS__);	\
+		}	\
+	} while (0)
+
+#define RM_LOG_ERR(fmt, ...)	\
+	do {	\
+		if (__ratelimit(&msg_log_rs)) {	\
+			printk(KERN_ERR pr_fmt(fmt), ##__VA_ARGS__);	\
+		}	\
+	} while (0)
+
+#define SMH_MESSAGE_BUFFER_LENGTH	256
+#define SMH_MESSAGE_BUFFER_MAX_LENGTH	4096
+
+
+static int smh_message_buffer_length = SMH_MESSAGE_BUFFER_LENGTH;
+module_param(smh_message_buffer_length, int, 0444);
+
+/**
+ * FIND_AND_REMOVE_TIMEOUT_FROM_LIST - Macro to find and remove message from list
+ * @handle: Pointer to store found handle
+ * @lock: Spinlock to protect the list
+ * @list_head: List head to search
+ * @member: List member name in the structure
+ * @msgid_target: Target message ID to find
+ * @found: Boolean to indicate if message was found
+ *
+ * This macro searches for a message in the list by ID, removes timeout messages,
+ * and returns the found message handle.
+ */
+#define FIND_AND_REMOVE_TIMEOUT_FROM_LIST(handle, lock, list_head, member, msgid_target, found) \
+	do { \
+		spin_lock(lock); \
+		{ \
+			typeof(handle) __cur, __tmp; \
+			list_for_each_entry_safe(__cur, __tmp, list_head, member) { \
+				if (check_msg_is_timeout(&__cur->msg)) { \
+					list_del(&__cur->member); \
+					kfree(__cur); \
+					handle = NULL; \
+					continue; \
+				} \
+				if (__cur->msg.msgid == (msgid_target)) { \
+					found = true; \
+					list_del(&__cur->member); \
+					handle = __cur; \
+					break; \
+				} \
+			} \
+		} \
+		spin_unlock(lock); \
+	} while (0)
+
+struct smh_msg_handler {
+	struct sentry_msg_helper_msg msg;
+	bool ack;
+	struct list_head ack_list;
+	struct list_head get_list;
+};
+
+struct smh_msg_ctx {
+	struct kfifo msgbuf_send;
+	spinlock_t msgbuf_send_lock;
+
+	struct list_head msgbuf_ack;
+	spinlock_t msgbuf_ack_lock;
+
+	struct list_head msgbuf_get;
+	spinlock_t msgbuf_get_lock;
+
+	struct wait_queue_head user_wq;
+};
+
+static struct smh_msg_ctx msg_ctx;
+static atomic64_t message_id_generator; /* [1, message_id_generator] */
+
+/**
+ * smh_get_new_msg_id - Generate a new unique message ID
+ *
+ * Return: New message ID
+ */
+uint64_t smh_get_new_msg_id(void)
+{
+	return atomic64_inc_return(&message_id_generator);
+}
+EXPORT_SYMBOL(smh_get_new_msg_id);
+
+/**
+ * check_msg_is_timeout - Check if message has timed out
+ * @msg: Message to check
+ *
+ * Return: true if timeout, false otherwise
+ */
+static bool check_msg_is_timeout(struct sentry_msg_helper_msg *msg)
+{
+	uint64_t now = ktime_get_ns();
+	uint64_t interval_time = (now - msg->start_send_time) / NSEC_PER_MSEC;
+
+	return interval_time > msg->timeout_time;
+}
+
+/**
+ * smh_message_send - Send a message through the message helper
+ * @msg: Message to send
+ * @ack: Whether acknowledgment is required
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+int smh_message_send(struct sentry_msg_helper_msg *msg, bool ack)
+{
+	int ret = 0;
+	struct smh_msg_handler *handle;
+
+	if (!msg->msgid) {
+		RM_LOG_ERR("please set the correct msgid by 'smh_get_new_msg_id', stop to send this msg\n");
+		return -EINVAL;
+	}
+
+	handle = kzalloc(sizeof(*handle), GFP_ATOMIC);
+	if (!handle) {
+		RM_LOG_ERR("failed to alloc message handle\n");
+		return -ENOMEM;
+	}
+
+	handle->msg = *msg;
+	handle->ack = ack;
+
+	RM_LOG_INFO("smh_message_send: %llu start\n", msg->msgid);
+
+	ret = kfifo_in_spinlocked(&msg_ctx.msgbuf_send, &handle,
+				  sizeof(handle), &msg_ctx.msgbuf_send_lock);
+	if (!ret) {
+		RM_LOG_ERR("error sending message %llu: buffer is full; message dropped\n",
+		       msg->msgid);
+		kfree(handle);
+		return -EAGAIN;
+	}
+
+	/* Check if someone is waiting */
+	if (waitqueue_active(&msg_ctx.user_wq))
+		wake_up(&msg_ctx.user_wq);
+
+	RM_LOG_INFO("smh_message_send: %llu end\n", msg->msgid);
+
+	return 0;
+}
+EXPORT_SYMBOL(smh_message_send);
+
+/**
+ * smh_message_get - Get a message from the message helper
+ * @buf: User space buffer to copy message to
+ *
+ * Return: Number of bytes copied on success, negative error code on failure
+ */
+ssize_t smh_message_get(void __user *buf)
+{
+	int ret;
+	struct smh_msg_handler *handle = NULL;
+	struct smh_msg_handler *handle_ack;
+	DEFINE_WAIT(wait);
+
+	if (waitqueue_active(&msg_ctx.user_wq)) {
+		RM_LOG_WARN("another process is waiting for message\n");
+		return -EPERM;
+	}
+
+	do {
+		ret = kfifo_out_spinlocked(&msg_ctx.msgbuf_send, &handle,
+					   sizeof(handle), &msg_ctx.msgbuf_send_lock);
+		if (ret) {
+			if (check_msg_is_timeout(&handle->msg)) {
+				RM_LOG_INFO("smh_message_get: %llu timeout\n", handle->msg.msgid);
+				kfree(handle);
+				handle = NULL;
+				continue;
+			}
+			break;
+		}
+
+		add_wait_queue_exclusive(&msg_ctx.user_wq, &wait);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+		set_current_state(TASK_RUNNING);
+		remove_wait_queue(&msg_ctx.user_wq, &wait);
+		if (signal_pending(current)) {
+			RM_LOG_ERR("error reading message: process receive signal\n");
+			return -ERESTART;
+		}
+	} while (1);
+
+	if (!handle)
+		return -ENOMSG;
+
+	RM_LOG_INFO("smh_message_get: get msg, msgid is %llu\n", handle->msg.msgid);
+
+	ret = copy_to_user(buf, &handle->msg, sizeof(handle->msg));
+	if (ret) {
+		RM_LOG_ERR("%s: failed to copy message to user: %d\n", __func__, ret);
+		ret = kfifo_in_spinlocked(&msg_ctx.msgbuf_send, &handle,
+					  sizeof(handle), &msg_ctx.msgbuf_send_lock);
+		if (!ret) {
+			RM_LOG_ERR("error recover message %llu: buffer is full; message dropped\n",
+			       handle->msg.msgid);
+			kfree(handle);
+			return -EFAULT;
+		}
+		return -EAGAIN;
+	}
+
+	if (handle->ack) {
+		bool found = false;
+
+		spin_lock(&msg_ctx.msgbuf_ack_lock);
+		list_for_each_entry(handle_ack, &msg_ctx.msgbuf_ack, ack_list) {
+			if (handle_ack->msg.msgid == handle->msg.msgid) {
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+			list_add_tail(&handle->ack_list, &msg_ctx.msgbuf_ack);
+		spin_unlock(&msg_ctx.msgbuf_ack_lock);
+	} else {
+		kfree(handle);
+	}
+
+	return sizeof(handle->msg);
+}
+
+/**
+ * smh_message_ack - Acknowledge a message
+ * @msg: Message to acknowledge
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+int smh_message_ack(struct sentry_msg_helper_msg *msg)
+{
+	struct smh_msg_handler *handle;
+	bool found = false;
+
+	RM_LOG_INFO("smh_message_ack: %llu\n", msg->msgid);
+
+	FIND_AND_REMOVE_TIMEOUT_FROM_LIST(handle, &msg_ctx.msgbuf_ack_lock,
+					  &msg_ctx.msgbuf_ack, ack_list,
+					  msg->msgid, found);
+
+	if (!found) {
+		RM_LOG_ERR("smh_message_ack: %llu not found, maybe this message is not exist or has been timeout\n",
+		       msg->msgid);
+		return -ENOENT;
+	}
+
+	handle->msg.res = msg->res;
+
+	spin_lock(&msg_ctx.msgbuf_get_lock);
+	list_add_tail(&handle->get_list, &msg_ctx.msgbuf_get);
+	spin_unlock(&msg_ctx.msgbuf_get_lock);
+
+	return 0;
+}
+
+/**
+ * smh_message_get_ack - Get acknowledgment for a message
+ * @msg: Message to get acknowledgment for
+ *
+ * Return: 1 if acknowledgment found, 0 otherwise
+ */
+int smh_message_get_ack(struct sentry_msg_helper_msg *msg)
+{
+	struct smh_msg_handler *handle;
+	bool found = false;
+
+	FIND_AND_REMOVE_TIMEOUT_FROM_LIST(handle, &msg_ctx.msgbuf_get_lock,
+					  &msg_ctx.msgbuf_get, get_list,
+					  msg->msgid, found);
+
+	if (found) {
+		msg->res = handle->msg.res;
+		kfree(handle);
+	}
+
+	return found;
+}
+EXPORT_SYMBOL(smh_message_get_ack);
+
+/**
+ * smh_message_init - Initialize the message helper subsystem
+ *
+ * Return: 0 on success, negative error code on failure
+ */
+int smh_message_init(void)
+{
+	int ret;
+
+	if (smh_message_buffer_length <= 0 ||
+	    smh_message_buffer_length > SMH_MESSAGE_BUFFER_MAX_LENGTH) {
+		RM_LOG_ERR("invalid smh_message_buffer_length\n");
+		return -EINVAL;
+	}
+
+	ret = kfifo_alloc(&msg_ctx.msgbuf_send,
+			  sizeof(struct smh_msg_handler *) * smh_message_buffer_length,
+			  GFP_KERNEL);
+	if (ret < 0) {
+		RM_LOG_ERR("error allocating send message buffer: %d\n", ret);
+		return ret;
+	}
+	spin_lock_init(&msg_ctx.msgbuf_send_lock);
+
+	INIT_LIST_HEAD(&msg_ctx.msgbuf_ack);
+	spin_lock_init(&msg_ctx.msgbuf_ack_lock);
+
+	INIT_LIST_HEAD(&msg_ctx.msgbuf_get);
+	spin_lock_init(&msg_ctx.msgbuf_get_lock);
+
+	init_waitqueue_head(&msg_ctx.user_wq);
+	atomic64_set(&message_id_generator, 0);
+
+	return 0;
+}
+
+/**
+ * smh_message_exit - Cleanup the message helper subsystem
+ */
+void smh_message_exit(void)
+{
+	struct smh_msg_handler *handle, *tmp;
+
+	/* Clean up acknowledgment list */
+	spin_lock(&msg_ctx.msgbuf_ack_lock);
+	list_for_each_entry_safe(handle, tmp, &msg_ctx.msgbuf_ack, ack_list) {
+		list_del(&handle->ack_list);
+		kfree(handle);
+	}
+	spin_unlock(&msg_ctx.msgbuf_ack_lock);
+
+	/* Clean up get list */
+	spin_lock(&msg_ctx.msgbuf_get_lock);
+	list_for_each_entry_safe(handle, tmp, &msg_ctx.msgbuf_get, get_list) {
+		list_del(&handle->get_list);
+		kfree(handle);
+	}
+	spin_unlock(&msg_ctx.msgbuf_get_lock);
+
+	kfifo_free(&msg_ctx.msgbuf_send);
+}

arch/arm64/configs/openeuler_defconfig
@@ -8388,6 +8388,10 @@ CONFIG_UDFI_ODF=m
 CONFIG_FWCTL_UB=m
 # end of UB_FWCTL
 
+# UB sentry
+CONFIG_UB_SENTRY=m
+# end of UB sentry
+
 #
 # PAGEATTACH SUPPORT
 #

drivers/ub/Kconfig
@@ -19,6 +19,7 @@ source "drivers/ub/ubase/Kconfig"
 source "drivers/ub/cdma/Kconfig"
 source "drivers/ub/urma/hw/udma/Kconfig"
 source "drivers/ub/obmm/Kconfig"
+source "drivers/ub/sentry/Kconfig"
 
 config UB_URMA
 	tristate "Unified Bus (UB) urma support"

drivers/ub/Makefile
@@ -7,3 +7,4 @@ obj-$(CONFIG_UB_UBASE) += ubase/
 obj-$(CONFIG_UB_CDMA) += cdma/
 obj-$(CONFIG_UB_URMA) += urma/
 obj-$(CONFIG_UB_UDMA) += urma/hw/udma/
+obj-$(CONFIG_UB_SENTRY) += sentry/

drivers/ub/sentry/Kconfig
@@ -1,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+config UB_SENTRY
+    tristate "sentry message report"
+    depends on UB && ACPI_POWER_NOTIFIER_CHAIN
+    default m
+    help
+      Listens to kernel event(eg. oom) and send sentry msg to userspace
+      Provides device for userspace to read kernel message and reply ack
+      Provides kernel api to send message to userspace and wait for result

drivers/ub/sentry/Makefile
@@ -1,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Makefile for the sentry drivers.
+
+obj-$(CONFIG_UB_SENTRY) += sentry_msg_helper.o
+obj-$(CONFIG_UB_SENTRY) += sentry_reporter.o
+
+sentry_msg_helper-y := smh_core.o smh_message.o

kunpengcompute/ub-os-component-dkms

commit ba866a1ba7a909eeb86e32484892e32823ad53de

drivers/ub/sentry: fix memory leak and use-after-free in smh_message_get()
drivers inclusion
category: bugfix
bugzilla: https://atomgit.com/openeuler/kernel/issues/9385

(cherry-pick from OLK-6.6 commit cc1495f9607d)

--------------------------------

When handle->ack is true and a matching msgid is already found in the
msgbuf_ack list, the handle was neither added to the list nor freed,
causing a memory leak. Add kfree(handle) in the "found" branch to
release the duplicate handle.

Additionally, the return statement `sizeof(handle->msg)` referenced
handle after it could have been freed in both the ack and non-ack
paths. Save the message size to a local variable (msg_size) before
the potential kfree and return it instead, fixing the use-after-free.

Fixes: 1e3e4971f8d7 ("drivers/ub/sentry: add sentry_reporter and sentry_msg_help")
Signed-off-by: Deng Guangxing <dengguangxing@huawei.com>
Signed-off-by: shixuantong <shixuantong1@huawei.com>
Signed-off-by: chenjiayi <chenjiayi22@huawei.com>
Signed-off-by: Wang Hai <wanghai38@huawei.com>

drivers/ub/sentry/smh_message.c
@@ -194,6 +194,7 @@ EXPORT_SYMBOL(smh_message_send);
 ssize_t smh_message_get(void __user *buf)
 {
 	int ret;
+	ssize_t msg_size;
 	struct smh_msg_handler *handle = NULL;
 	struct smh_msg_handler *handle_ack;
 	DEFINE_WAIT(wait);
@@ -246,6 +247,7 @@ ssize_t smh_message_get(void __user *buf)
 		return -EAGAIN;
 	}
 
+	msg_size = sizeof(handle->msg);
 	if (handle->ack) {
 		bool found = false;
 
@@ -258,12 +260,14 @@ ssize_t smh_message_get(void __user *buf)
 		}
 		if (!found)
 			list_add_tail(&handle->ack_list, &msg_ctx.msgbuf_ack);
+		else
+			kfree(handle);
 		spin_unlock(&msg_ctx.msgbuf_ack_lock);
 	} else {
 		kfree(handle);
 	}
 
-	return sizeof(handle->msg);
+	return msg_size;
 }
 
 /**